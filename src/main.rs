fe5891d1466c32378853e7bd3c6946bb66913a69
Joseph <Jaghoghov@gmail.com>
2024-12-27 01:29:04 +0100

    feat: Added true nature

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use dioxus::prelude::*;
 
+static CSS: Asset = asset!("/assets/main.css");
+
 fn main() {
     dioxus::launch(App);
 }
@@ -7,6 +9,28 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-    "Hello World!"
+        document::Stylesheet { href: CSS }
+        DogApp { breed: "corgi" }
+    }
+}
+
+#[derive(Props, PartialEq, Clone)]
+struct DogProps {
+    breed: String,
+}
+
+#[component]
+fn DogApp(props: DogProps) -> Element {
+    rsx! {
+        div { id: "title",
+            h1 { "Joseph Aghoghovbia" },
+        }
+        div { id: "dogview",
+            img { src: "https://media1.tenor.com/m/lZuEjo0EGW4AAAAd/snowman-mewing.gif" }
+        }
+        div { id: "buttons",
+            button { id: "skip", "skip" },
+            button { id: "save", "save!" }
+        }
     }
 }
